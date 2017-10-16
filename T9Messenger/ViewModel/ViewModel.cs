89debd093d9e9d9f0f0b6f2631242fff4cdea211
Stephen <syk4367@rit.edu>
2017-10-16 16:30:40 -0400

    Creating GUI

---
+++ T9Messenger/ViewModel/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace T9Messenger.ViewModel
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
