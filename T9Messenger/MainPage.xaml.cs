48ed1dc5523c9e4987ba51227806aabfb3be6581
Stephen <syk4367@rit.edu>
2017-10-17 21:56:16 -0400

    deleted code mentioned from previous commit

--- T9Messenger/MainPage.xaml.cs
+++ T9Messenger/MainPage.xaml.cs
@@ -43,35 +43,27 @@ namespace T9Messenger
                     // nothing
                     break;
                 case "2":
-                    //do_keypress(2, new char[] { 'A', 'B', 'C' });
                     vm.keypress(2);
                     break;
                 case "3":
-                    //do_keypress(3, new char[] { 'D', 'E', 'F' });
                     vm.keypress(3);
                     break;
                 case "4":
-                    //do_keypress(4, new char[] { 'G', 'H', 'I' });
                     vm.keypress(4);
                     break;
                 case "5":
-                    //do_keypress(5, new char[] { 'J', 'K', 'L' });
                     vm.keypress(5);
                     break;
                 case "6":
-                    //do_keypress(6, new char[] { 'M', 'N', 'O' });
                     vm.keypress(6);
                     break;
                 case "7":
-                    //do_keypress(7, new char[] { 'P', 'Q', 'R', 'S' });
                     vm.keypress(7);
                     break;
                 case "8":
-                    //do_keypress(8, new char[] { 'T', 'U', 'V' });
                     vm.keypress(8);
                     break;
                 case "9":
-                    //do_keypress(9, new char[] { 'W', 'X', 'Y', 'Z' });
                     vm.keypress(9);
                     break;
                 case "*":
@@ -86,51 +78,6 @@ namespace T9Messenger
             //vm.text = "1";
         }
 
-        private void do_keypress(int keyCode, char[] chars)
-        {
-            if (!vm.predictive)
-            {
-                // was less than a second since last keypress 
-                if ((DateTime.Now - vm.lastPress).Seconds < 1 && vm.keyCode == keyCode)
-                {
-                    char lastch = vm.text[vm.text.Length - 1];
-                    for (int i = 0; i < chars.Length; i++)
-                    {
-                        if (lastch == chars[i])
-                        {
-                            int idx = (i < chars.Length - 1) ? i + 1 : 0;
-                            vm.text = vm.text.Substring(0, vm.text.Length - 1) + chars[idx];
-                            break;
-                        }
-                    }
-                }
-                // add new letter to text 
-                else
-                {
-                    vm.text += chars[0];
-                }
-
-                vm.lastPress = DateTime.Now;
-                vm.keyCode = keyCode;
-            }
-            else
-            {
-                var last = (vm.keyCombs.Count != 0) ? vm.keyCombs.Last() : new List<int>();
-                last.Add(keyCode);
-                // new list, add it
-                if(last.Count == 1)
-                {
-                    vm.keyCombs.Add(last);
-                }
-                var ws = possible_valid_words(last);
-                foreach ( var w in ws)
-                {
-                    Debug.Write(w + " ");
-                }
-                Debug.WriteLine("");
-            }
-        }
-
         private void Button_ClickSpace(object sender, RoutedEventArgs e)
         {
             vm.text += " ";
@@ -142,49 +89,6 @@ namespace T9Messenger
             {
                 vm.text = vm.text.Substring(0, vm.text.Length - 1);
             }
-        }
-
-        private List<string> possible_words(IEnumerable<int> comb)
-        {
-            var letters = ViewModel.ViewModel.keyMap[comb.ElementAt(0)];
-            List<string> words = new List<string>();
-            if (comb.Count() == 1)
-            {
-                foreach(var c in letters)
-                {
-                    words.Add(c.ToString());
-                }
-                return words;
-            }
-            else
-            {
-                foreach (var c in letters)
-                {
-                    var tail = comb.Skip(1);
-                    var sub_words = possible_words(tail);
-                    foreach( var w in sub_words )
-                    {
-                        words.Add(c + w);   
-                    }
-
-                }
-            }
-            //List<string> words = new List<string>();
-            return words;
-        }
-
-        private List<string> possible_valid_words(IEnumerable<int> comb)
-        {
-            List<string> valid_words = new List<string>();
-            var words = possible_words(comb);
-            foreach ( var w in words)
-            {
-                if ( vm.m.words.Contains(w))
-                {
-                    valid_words.Add(w);
-                }
-            }
-            return valid_words;
         }
     }
 }
